6b51cc9f1b439d9b7d9440b1144312bb184f62fc
Ubuntu <ubuntu@ip-172-31-34-3.ec2.internal>
2019-07-29 15:08:35 +0000

    Added postdetail page and included commenting functionality

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import get_all, get_features, get_bugs
+from .views import get_all, get_features, get_bugs, post_detail
 
 urlpatterns = [
     url(r'^$', get_all, name='get_all'),
     url(r'^features/$', get_features, name='get_features'),
     url(r'^bugs/$', get_bugs, name='get_bugs'),
+    url(r'^(?P<pk>\d+)/$', post_detail, name='post_detail'),
 ]
