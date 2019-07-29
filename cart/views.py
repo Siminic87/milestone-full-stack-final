fd948a4cfe1de095895c3753b925cf338cf76029
Ubuntu <ubuntu@ip-172-31-34-3.ec2.internal>
2019-07-29 16:13:40 +0000

    Initialized cart app and wrote logic for cart in views.py

---
+++ cart/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect, reverse
+
+# Cart
+def view_cart(request):
+    """A view that renders the carts content page"""
+    return render(request, "cart.html")
+    
+def add_to_cart(request, id):
+    """Add a quantity of the specified product to the cart"""
+    quantity = int(request.POST.get('quantity'))
+    
+    cart = request.session.get('cart', {})
+    cart[id] = cart.get(id, quantity)
+    
+    request.session['cart'] = cart
+    return redirect(reverse('get_features'))
+    
+def adjust_cart(request, id):
+    """Adjust the quantity of the specified product to the specified amount"""
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+        
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
