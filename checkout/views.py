c4fcaf65a9ab3d65206cfec32eb89ad904796c39
Ubuntu <ubuntu@ip-172-31-34-3.ec2.internal>
2019-07-29 16:47:53 +0000

    Initialized and wrote logic for checkout app

---
+++ checkout/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import MakePaymentForm, OrderForm
+from .models import OrderLineItem
+from django.conf import settings
+from django.utils import timezone
+from posts.models import Post, Voter
+import stripe
+
+stripe.api_key = settings.STRIPE_SECRET
+
+#Checkout
+@login_required()
+def checkout(request):
+    """
+    Checks if order form is valid and transmits payment to stripe. If payment 
+    is successful, upvotes for chosen feature request are incremented by chosen
+    quantity.
+    """
+    if request.method=="POST":
+        order_form = OrderForm(request.POST)
+        payment_form = MakePaymentForm(request.POST)
+        
+        if order_form.is_valid() and payment_form.is_valid():
+            order = order_form.save(commit=False)
+            order.date = timezone.now()
+            order.save()
+            
+            cart = request.session.get("cart", {})
+            total = 0
+            for id, quantity in cart.items():
+                post = get_object_or_404(Post, pk=id)
+                total += quantity * post.price
+                order_line_item = OrderLineItem(
+                    order = order,
+                    post = post,
+                    quantity = quantity
+                    )
+                order_line_item.save()
+                
+                try:
+                    customer = stripe.Charge.create(
+                        amount = int(total * 100),
+                        currency = "EUR",
+                        description = request.user.email,
+                        card = payment_form.cleaned_data['stripe_id'],
+                        )
+                except stripe.error.CardError:
+                    messages.error(request, "Your card was declined!")
+                    
+                if customer.paid:
+                    messages.error(request, "You have successfully paid!")
+                    request.session['cart'] = {}
+                    
+                    # increment upvotes by "quantity".
+                    post.upvotes += quantity
+                    post.save()
+                    
+                    return redirect(reverse('get_all'))
+                else:
+                    messages.error(request, "Unable to make payment")
+        else:
+            print(payment_form.errors)
+            messages.error(request, "We were unable to take a payment with that card!")
+    else:
+        payment_form = MakePaymentForm()
+        order_form = OrderForm()
+        
+    return render(request, "checkout.html", {'order_form':order_form, 'payment_form':payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})
