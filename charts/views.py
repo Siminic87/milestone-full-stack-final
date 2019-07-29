e6b9b81fab6ee57be018aec25f9462ce0c88aa8c
Ubuntu <ubuntu@ip-172-31-34-3.ec2.internal>
2019-07-29 17:11:59 +0000

    removed authentication methods from chart logic and inserted right methods

--- charts/views.py
+++ charts/views.py
@@ -1,62 +1,38 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect
-from django.contrib import messages, auth
-from django.core.urlresolvers import reverse
-from .forms import UserLoginForm, UserRegistrationForm
-from django.template.context_processors import csrf
-from django.contrib.auth.decorators import login_required
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# User authentication
-def logout(request):
-    """A view that logs the user out and redirects back to the index page"""
-    auth.logout(request)
-    messages.success(request, 'You have successfully logged out')
-    return redirect(reverse('get_all'))
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.utils import timezone
+from datetime import timedelta
+from posts.models import Post, Voter
 
+### Summary Charts ###
+class ChartData(APIView):
+    """
+    View to query database for average of bugs and feature requests that have 
+    been "done" per day, week and month. 
+    """
+    authentication_classes = []
+    permission_classes = []
 
-def login(request):
-    """A view that manages the login form"""
-    if request.method == 'POST':
-        user_form = UserLoginForm(request.POST)
-        if user_form.is_valid():
-            user = auth.authenticate(request.POST['username_or_email'],
-                                     password=request.POST['password'])
+    def get(self, request, format=None):
+        labels = ['Day', 'Week', 'Month']
+        default_items = [Post.objects.filter(type="Feature Request", status="Done", published_date__gte=timezone.now()-timedelta(30)).count() / 30,
+                        Post.objects.filter(type="Feature Request", status="Done", published_date__gte=timezone.now()-timedelta(28)).count() / 4,
+                        Post.objects.filter(type="Feature Request", status="Done", published_date__gte=timezone.now()-timedelta(30)).count()]
+        default_items2 = [Post.objects.filter(type="Bug", status="Done", published_date__gte=timezone.now()-timedelta(30)).count() / 30,
+                        Post.objects.filter(type="Bug", status="Done", published_date__gte=timezone.now()-timedelta(28)).count() / 4,
+                        Post.objects.filter(type="Bug", status="Done", published_date__gte=timezone.now()-timedelta(30)).count()]
+        data = {
+            "labels": labels,
+            "default": default_items,
+            "default2": default_items2,
+        }
+        return Response(data)
 
-            if user:
-                auth.login(request, user)
-                messages.error(request, "You have successfully logged in!")
-
-                if request.GET and request.GET['next'] !='':
-                    next = request.GET['next']
-                    return HttpResponseRedirect(next)
-                else:
-                    return redirect(reverse('get_all'))
-            else:
-                user_form.add_error(None, "Your username or password are incorrect!")
-    else:
-        user_form = UserLoginForm()
-
-    args = {'user_form': user_form, 'next': request.GET.get('next', '')}
-    return render(request, 'login.html', args)
-
-def register(request):
-    """A view that manages the registration form"""
-    if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            user_form.save()
-
-            user = auth.authenticate(request.POST.get('email'),
-                                     password=request.POST.get('password1'))
-
-            if user:
-                auth.login(request, user)
-                messages.success(request, "You have successfully registered")
-                return redirect(reverse('get_all'))
-
-            else:
-                messages.error(request, "unable to log you in at this time!")
-    else:
-        user_form = UserRegistrationForm()
-
-    args = {'user_form': user_form}
-    return render(request, 'register.html', args)
+def charts(request):
+    """
+    Renders page with summary charts
+    """
+    return render(request, 'charts.html')
