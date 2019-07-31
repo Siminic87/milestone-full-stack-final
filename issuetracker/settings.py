5e75f45879dd746b1f5bf8d211fbf8dfec402f05
Ubuntu <ubuntu@ip-172-31-34-3.ec2.internal>
2019-07-31 10:31:36 +0000

    Corrected spelling of ALLOWED HOSTS

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('HOST_C9'), 'https://milestone-full-stack-final.herokuapp.com/']
+ALLOWED_HOSTS = [os.getenv('HOST_C9'), 'https://milestone-full-stack-final.herokuapp.com']
 
 # Application definition
 
